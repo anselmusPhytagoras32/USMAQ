55a197f78e2af077a817efa0b01b0ca59cf3bf70
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-01-04 02:46:44 +0800

    UPDATE: sample template for the tsx files

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,9 +1,9 @@
 function App(){
     return(
-        <main className = "primary-btn">
+        <main style={{ padding: '40px 0', textAlign: 'center' }}>
             <h1>USMAQ</h1>
+            <button className="primary-btn">Click Me</button>
         </main>
     );
 }
-
 export default App;
