fbcf22a80c1887ed5c55b9a769479bf77e826883
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-01-03 18:19:40 +0800

    UPDATE: Fixed classes path, and added spring boot in config

---
+++ src/main/java/com/core/usmaq/USMAQ_Application.java
@@ -0,0 +1,12 @@
+package com.core.usmaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class USMAQ_Application {
+    public static void main(String[] args) {
+        SpringApplication.run(USMAQ_Application.class, args);
+    }
+}
+
