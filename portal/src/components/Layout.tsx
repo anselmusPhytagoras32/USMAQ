67c38f84be82e0efdf280e3d076827ba5e455e26
anselmusPhytagoras32 <gebhel.anselm@gmail.com>
2026-01-09 00:19:01 +0800

    Added searchBar component and refactored the layout

--- portal/src/components/Layout.tsx
+++ portal/src/components/Layout.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Box, CssBaseline, AppBar, Toolbar, Typography } from '@mui/material';
+import { Box, CssBaseline, AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu'; // Import the icon
 import Sidebar from "./Sidebar.tsx";
 import AccountMenu from "@/components/AccountMenu.tsx";
+import SearchAppBar from "@/components/SearchBar.tsx";
 
 type LayoutProps = {
     children: React.ReactNode;
@@ -12,21 +14,40 @@ const Layout = ({ children }: LayoutProps) => {
         <Box sx={{ display: 'flex' }}>
             <CssBaseline />
 
-            {/* Top Header */}
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        USMAQ Hospital System
-                    </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <AccountMenu></AccountMenu>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
+                    {/* LEFT: Menu Icon + Title */}
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        {/* The 3 Lines (Menu Icon) */}
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+
+                        <Typography variant="h6" noWrap component="div">
+                            USMAQ Hospital System
+                        </Typography>
+                    </Box>
+
+                    {/* MIDDLE: Search Bar (Now it's just the input) */}
+                    <SearchAppBar />
+
+                    {/* RIGHT: Account Menu */}
+                    <AccountMenu />
+
                 </Toolbar>
             </AppBar>
 
             <Sidebar />
 
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <Toolbar /> {/* Spacer for the header */}
+                <Toolbar />
                 {children}
             </Box>
         </Box>
